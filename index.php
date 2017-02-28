8a91d875e6b75b2f342873c966d70dd0f55028c2
Chidinma Okafor <fildaokafor@gmail.com>
2017-02-28 16:41:18 +0000

    table

--- index.php
+++ index.php
@@ -1,11 +1,31 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <title>php table</title>
      </head>
      <body>
 <p>
 <?php
-echo"Hello World";
+$firstname1 ="chicomzee";
+$surname1 ="felix";
+
+$firstname2 ="collins";
+$surname2 ="christopher";
+
+$firstname3 ="nichol";
+$surname3="peters";
+
+$firstname4 ="lois";
+$surname4="uzor";
+
+$firstname5 ="stephnie";
+$surname5="paul";
+
+echo"<table border=1>";
+echo "<tr><td>firstname</td><td>surname</td></tr>";
+echo "<tr><td>firstname1</td><td>surname1</td></tr>";
+echo "</table>";
+
 ?>
 </p>
      </body>
